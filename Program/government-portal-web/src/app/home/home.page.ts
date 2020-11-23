a47294b988fdb4e2112d5bb34729105a6499a76a
Ranul Ladduwahetty <rpladduwahetty@students.nsbm.lk>
2020-11-23 13:57:15 +0530

    Backup [Recovery-1]
    - Windows file system crashed unexpectedly, working in progress would be uploaded to recover if the next system crash becomes impossible to recover.
    Notes: Work uploaded doesn't reflect a completed component but working in progress

--- Program/government-portal-web/src/app/home/home.page.ts
+++ Program/government-portal-web/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,33 +10,65 @@ export class HomePage {
   home: boolean;
   visitor: string;
   Welcome: any;
+  searchFilter: string;
   slideOpts = {
     initialSlide: 0,
     slidesPerView: 1,
     autoplay: true,
-    speed: 400
+    speed: 3000
   };
   constructor() { }
   ngOnInit() {
-    this.loading = false; // loading disabled during development
-    this.home = true; // delete this line and set this.loading to true
-    // setTimeout(() => {
-    //   this.loading = false;
-    //   this.home = true;
-    // }, 5000);
-    // if (localStorage.getItem('Visitor') == null) {
-      localStorage.setItem('Visitor', 'false');
+    this.loading = false; // loading disabled during development set this.loading to true
+    setTimeout(() => {
+      if (localStorage.getItem('Visitor') == null) {
+        localStorage.setItem('Visitor', 'false');
+        this.loading = false;
+        this.home = false;
+        this.Welcome = true;
+        console.log("New user");
+      }
+      else if (localStorage.getItem('Visitor') == 'false') {
+      // this.loading = false;
+      // this.Welcome = false;
+      this.loading = false;
       this.Welcome = true;
-      this.home = false;
-      console.log("New user");
-    // }
-    // else if (localStorage.getItem('Visitor') == 'false') {
-    //   this.Welcome = false;
-    //   console.log("Old user" + localStorage.getItem('Visitor'));
-    // }
+      
+    this.home = true;
+        console.log("Old user" + localStorage.getItem('Visitor'));
+      }
+    }, 5000);
+    
   }
   goHome() {
     this.Welcome = false;
     this.home = true;
   }
+
+  availablePages = [{
+    "id": 1,
+    "name": "Loans",
+    "link": ""
+  },
+  {
+    "id": 2,
+    "name": "Taxes",
+    "link": ""
+  },
+  {
+    "id": 3,
+    "name": "NIC",
+    "link": ""
+  },
+  {
+    "id": 4,
+    "name": "Passports",
+    "link": ""
+  },
+  {
+    "id": 6,
+    "name": "Educational",
+    "link": ""
+  }
+]
 }
