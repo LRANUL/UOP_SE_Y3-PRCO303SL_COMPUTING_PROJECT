eea7a3c9e3e8ec930472edeb23283c671e71b175
Ranul Ladduwahetty <10673986@students.plymouth.ac.uk>
2021-05-02 23:39:21 +0530

    System v0.9.9
    - Secretary Upgrade completed
    - Upgrading Administrator

--- Program/Applications/government-portal-desktop-secretary/src/app/secretary/home-affairs/home-affairs.page.spec.ts
+++ Program/Applications/government-portal-desktop-secretary/src/app/secretary/home-affairs/home-affairs.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomeAffairsPage } from './home-affairs.page';
@@ -7,7 +7,7 @@ describe('HomeAffairsPage', () => {
   let component: HomeAffairsPage;
   let fixture: ComponentFixture<HomeAffairsPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomeAffairsPage ],
       imports: [IonicModule.forRoot()]
